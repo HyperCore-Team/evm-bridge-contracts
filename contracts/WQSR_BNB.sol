db346b0d83e738dd12772f67a0228c4dee6f47b7
sumoshi21 <su_moshi_1@proton.me>
2024-11-22 07:09:32 -0500

    add wznn & wqsr tokens for bnb chain deployment

---
+++ contracts/WQSR_BNB.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract WQSR is AccessControl, ERC20 {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor(address bridgeAddress) ERC20("Wrapped QSR", "wQSR") {
+        _grantRole(MINTER_ROLE, bridgeAddress);
+        _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    }
+
+    function decimals() public view override returns (uint8) {
+        return 8;
+    }
+
+    function mint(address account, uint256 value) onlyRole(MINTER_ROLE) external {
+        _mint(account, value);
+    }
+
+    function burn(uint value) external {
+        _burn(_msgSender(), value);
+    }
+}
